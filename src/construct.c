0fad22bbbafe868d2064b178f98e1f7f531b5f2c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-14 17:14:45 +0100

    implement `vector_new`

--- src/construct.c
+++ src/construct.c
@@ -1,3 +1,12 @@
 #include "t_vector.h"
 
-t_vector* vector_new(void);
+#include <stdlib.h>
+
+t_vector* vector_new(void)
+{
+	t_vector* new = malloc(sizeof(*new));
+	new->data = NULL;
+	new->len = 0;
+	new->capacity = 0;
+	return new;
+}
