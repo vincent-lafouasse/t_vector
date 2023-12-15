9d083afb9bac9f083b97e25d3922c75bdc6a128e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-15 18:23:01 +0100

    update tests

--- test/src/allocation.c
+++ test/src/allocation.c
@@ -1,6 +1,17 @@
 #include "t_vector.h"
 #include "unity.h"
 
+const int NUMS[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+static void* digit_ptr(int digit, const int* nums)
+{
+	return (void*)(nums + digit);
+}
+
+static int to_int(void* int_ptr)
+{
+	return *(int*)int_ptr;
+}
 
 static void test_default_constructor(void)
 {
@@ -10,7 +21,7 @@ static void test_default_constructor(void)
 	TEST_ASSERT(v->size == 0);
 	TEST_ASSERT(v->capacity == 0);
 
-	vec_delete(v);
+	//vec_delete(v);
 }
 
 static void test_constructor_with_size(void)
@@ -24,13 +35,13 @@ static void test_constructor_with_size(void)
 	TEST_ASSERT(v->size == 42);
 	TEST_ASSERT(v->capacity == 42);
 
-	vec_delete(v);
+	//vec_delete(v);
 }
 
 static void test_constructor_with_size_and_value(void)
 {
 	const int NUMS[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	t_vector* v = vec_new_init(42, (void*)(NUMS + 3));
+	t_vector* v = vec_new_init(42, digit_ptr(3, NUMS));
 	
 	if (!v)
 		TEST_FAIL();
@@ -44,13 +55,19 @@ static void test_constructor_with_size_and_value(void)
 		TEST_ASSERT(v->data[i] == NUMS + 3);
 	}
 
-	vec_delete(v);
+	//vec_delete(v);
 }
 
 static void test_constructor_from_array(void)
 {
 	const int NUMS[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	void* array[] = {NUMS + 1, NUMS + 3, NUMS + 5, NUMS, NUMS + 4};
+	void* array[] = {
+		digit_ptr(3, NUMS),
+		digit_ptr(0, NUMS),
+		digit_ptr(5, NUMS),
+		digit_ptr(2, NUMS),
+		digit_ptr(8, NUMS),
+	};
 	size_t size = sizeof(array)/sizeof(void*);
 
 	t_vector* v = vec_new_from_array(array, size);
@@ -61,11 +78,11 @@ static void test_constructor_from_array(void)
 	for (size_t i = 0; i < size; i++)
 	{
 		char error[100] = "";
-		sprintf(error, "error found at index %zu, expected %d was %d\n", i, *(int*)(v->data[i]), *(int*)(array[i]));
+		sprintf(error, "error found at index %zu, expected %d was %d\n", i, to_int(v->data[i]), to_int(array[i]));
 		TEST_ASSERT_MESSAGE(v->data[i] == array[i], error);
 	}
 
-	vec_delete(v);
+	//vec_delete(v);
 }
 
 void run_test_constructor(void)
