6d23c51f8824f06dfc0dc462318ea3798e41d19d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-15 15:00:07 +0100

    tester hello world

--- test/src/test.c
+++ test/src/test.c
@@ -1,8 +1,14 @@
 #include "t_vector.h"
+#include "unity.h"
+
 #include <stdio.h>
+
+void setUp(void) {}
+
+void tearDown(void) {}
 
 int main(void)
 {
-	t_vector* vec = vector_new();
-	printf("hello\n");
+    UnityBegin("t_vector");
+    return (UnityEnd());
 }
