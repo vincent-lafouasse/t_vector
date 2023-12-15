062bfdba668a8429194d47a9ef6d3b7aa0c950e0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-15 15:54:15 +0100

    construct vector with reserved size

--- src/construct.c
+++ src/construct.c
@@ -5,8 +5,22 @@
 t_vector* vector_new(void)
 {
 	t_vector* new = malloc(sizeof(*new));
+	if (!new)
+		return NULL;
 	new->data = NULL;
 	new->len = 0;
 	new->capacity = 0;
+	return new;
+}
+
+t_vector* 	vector_new_with_capacity(size_t capacity__)
+{
+	t_vector* new = vector_new();
+	if (!new)
+		return NULL;
+	new->capacity = capacity__;
+	new->data = malloc(capacity__ * sizeof(int));
+	if (!new->data)
+		return free(new), NULL;
 	return new;
 }
--- test/src/allocation.c
+++ test/src/allocation.c
@@ -10,8 +10,22 @@ static void test_default_constructor(void)
 	TEST_ASSERT(v->capacity == 0);
 }
 
+static void test_constructor_with_size(void)
+{
+	t_vector* v = vector_new_with_capacity(42);
+	
+	if (!v)
+		TEST_FAIL();
+
+	TEST_ASSERT(v->data != NULL);
+	TEST_ASSERT(v->len == 0);
+	TEST_ASSERT(v->capacity == 42);
+
+}
+
 void run_test_constructor(void)
 {
     printf("\n----- Allocation -----\n");
     RUN_TEST(test_default_constructor);
+    RUN_TEST(test_constructor_with_size);
 }
