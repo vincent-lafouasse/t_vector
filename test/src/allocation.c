2c6de188e3c57d724e06566bcb1fb834d99f5571
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-15 15:06:19 +0100

    restructure

---
+++ test/src/allocation.c
@@ -0,0 +1,13 @@
+#include "t_vector.h"
+#include "unity.h"
+
+static void test_default_constructor(void)
+{
+
+}
+
+void run_test_constructor(void)
+{
+    printf("\n----- Allocation -----\n");
+    RUN_TEST(test_default_constructor);
+}
--- test/src/main.c
+++ test/src/main.c
@@ -7,8 +7,11 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
+void run_test_constructor(void);
+
 int main(void)
 {
     UnityBegin("t_vector");
+	run_test_constructor();
     return (UnityEnd());
 }
