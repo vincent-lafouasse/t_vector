93e23b9947fd324bec95eb259e83f161aa9586ba
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-15 16:19:33 +0100

    test constructors

--- test/src/allocation.c
+++ test/src/allocation.c
@@ -23,6 +23,20 @@ static void test_constructor_with_size(void)
 
 }
 
+
+static void test_constructor_with_capacity(void)
+{
+	t_vector* v = vector_new_with_capacity(42);
+	
+	if (!v)
+		TEST_FAIL();
+
+	TEST_ASSERT(v->data != NULL);
+	TEST_ASSERT(v->size == 0);
+	TEST_ASSERT(v->capacity == 42);
+
+}
+
 static void test_constructor_with_size_and_value(void)
 {
 	t_vector* v = vector_new_init(42, 69);
@@ -40,10 +54,30 @@ static void test_constructor_with_size_and_value(void)
 	}
 }
 
+static void test_constructor_from_array(void)
+{
+	int array[5] = {1, 3, 5, 69, 420};
+	size_t size = sizeof(array)/sizeof(int);
+
+	t_vector* v = vector_new_from_array(array, size);
+	
+	if (!v)
+		TEST_FAIL();
+
+	for (size_t i = 0; i < size; i++)
+	{
+		char error[100] = "";
+		sprintf(error, "error found at index %zu, expected %d was %d\n", i, v->data[i], array[i]);
+		TEST_ASSERT_MESSAGE(v->data[i] == array[i], error);
+	}
+}
+
 void run_test_constructor(void)
 {
     printf("\n----- Allocation -----\n");
     RUN_TEST(test_default_constructor);
     RUN_TEST(test_constructor_with_size);
     RUN_TEST(test_constructor_with_size_and_value);
+    RUN_TEST(test_constructor_with_capacity);
+    RUN_TEST(test_constructor_from_array);
 }
