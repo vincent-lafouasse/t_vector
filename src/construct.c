c020f5b3e1b139d9317283f304fa6b0e96561d20
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-15 16:01:16 +0100

    different constructors

--- src/construct.c
+++ src/construct.c
@@ -19,8 +19,21 @@ t_vector* 	vector_new_with_capacity(size_t capacity__)
 	if (!new)
 		return NULL;
 	new->capacity = capacity__;
+	new->len = capacity__;
 	new->data = malloc(capacity__ * sizeof(int));
 	if (!new->data)
 		return free(new), NULL;
+	return new;
+}
+
+t_vector* 	vector_new_with_capacity_with_value(size_t capacity__, int value)
+{
+	t_vector* new = vector_new_with_capacity(capacity__);
+	if (!new)
+		return NULL;
+
+	for (size_t i = 0; i < capacity__; i++)
+		new->data[i] = value;
+
 	return new;
 }
--- test/src/allocation.c
+++ test/src/allocation.c
@@ -18,9 +18,26 @@ static void test_constructor_with_size(void)
 		TEST_FAIL();
 
 	TEST_ASSERT(v->data != NULL);
-	TEST_ASSERT(v->len == 0);
+	TEST_ASSERT(v->len == 42);
 	TEST_ASSERT(v->capacity == 42);
 
+}
+
+static void test_constructor_with_size_and_value(void)
+{
+	t_vector* v = vector_new_with_capacity_with_value(42, 69);
+	
+	if (!v)
+		TEST_FAIL();
+
+	TEST_ASSERT(v->data != NULL);
+	TEST_ASSERT(v->len == 42);
+	TEST_ASSERT(v->capacity == 42);
+
+	for (size_t i = 0; i < 42; i++)
+	{
+		TEST_ASSERT(v->data[i] == 69);
+	}
 }
 
 void run_test_constructor(void)
@@ -28,4 +45,5 @@ void run_test_constructor(void)
     printf("\n----- Allocation -----\n");
     RUN_TEST(test_default_constructor);
     RUN_TEST(test_constructor_with_size);
+    RUN_TEST(test_constructor_with_size_and_value);
 }
